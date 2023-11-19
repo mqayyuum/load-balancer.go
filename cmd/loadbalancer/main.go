ba94cc74d4ae0222782b5d850c11e81e59b65494
Qayyuum Harun <qayyuum.harun@outlook.com>
2023-11-19 22:02:42 +0100

    feat: handle concurrent request

--- cmd/loadbalancer/main.go
+++ cmd/loadbalancer/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
@@ -12,14 +13,37 @@ import (
 )
 
 var (
-	mutex    sync.Mutex
-	backends []string
+	mutex           sync.Mutex
+	backends        []string
+	healthyBackends []string
 )
 
 var currentBackend = 0
 
-func checkHealth(backend string, healthCh chan<- string) {
-	healthCh <- backend
+type HealthStatus struct {
+	Address string
+	Healthy bool
+}
+
+func checkHealth(backend string, healthCh chan<- HealthStatus) {
+	client := &http.Client{
+		Timeout: time.Second * 5,
+	}
+
+	resp, err := client.Get(backend + "/health")
+	if err != nil {
+		log.Printf("Health check failed for server %s: %v\n", backend, err)
+		healthCh <- HealthStatus{Address: backend, Healthy: false}
+		return
+	}
+
+	defer resp.Body.Close()
+
+	health := resp.StatusCode == http.StatusOK
+	healthCh <- HealthStatus{Address: backend, Healthy: health}
+	if !health {
+		log.Printf("Server %s is not healthy. Status Code %d\n", backend, resp.StatusCode)
+	}
 }
 
 func isHealthy(server string) bool {
@@ -34,28 +58,40 @@ func isHealthy(server string) bool {
 	return resp.StatusCode == http.StatusOK
 }
 
-var healthyBackends []string
-
 func updateHealthyBackends() {
-	healthyCh := make(chan string)
+	healthyCh := make(chan HealthStatus)
 
 	for {
 		for _, backend := range backends {
 			go checkHealth(backend, healthyCh)
 		}
 
-		select {
-		case backend := <-healthyCh:
-			// Update the list of healthy backends
+		for range backends {
+			status := <-healthyCh
 			mutex.Lock()
-			healthyBackends = append(healthyBackends, backend)
-			mutex.Unlock()
-		case <-time.After(10 * time.Second):
-			// Timeout: clear the list and start over
-			mutex.Lock()
-			healthyBackends = []string{}
+			if status.Healthy {
+				found := false
+				for _, b := range healthyBackends {
+					if b == status.Address {
+						found = true
+						break
+					}
+				}
+				if !found {
+					healthyBackends = append(healthyBackends, status.Address)
+				}
+
+			} else {
+				for i, backend := range healthyBackends {
+					if backend == status.Address {
+						healthyBackends = append(healthyBackends[:i], healthyBackends[i+1:]...)
+						break
+					}
+				}
+			}
 			mutex.Unlock()
 		}
+		time.Sleep(10 * time.Second)
 	}
 }
 
@@ -79,12 +115,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 	forwardReq(w, r)
 
 	fmt.Fprintf(w, "Received request from %s\n%s / %s\nHost: %s\nUser-Agent: %s\nAccept: %s", info.IP, info.Method, info.Protocol, info.Host, info.UserAgent, info.Accept)
-
 }
 
 func registerHandler(w http.ResponseWriter, r *http.Request) {
 	server := r.URL.Query().Get("server")
 	switch r.Method {
+	case http.MethodGet: // Get list of registered backend
+		w.Header().Set("Content-Type", "application/json")
+
+		if err := json.NewEncoder(w).Encode(backends); err != nil {
+			log.Printf("Error encoding response: %v", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+		}
+
 	case http.MethodPost: // Register a new backend
 		if server == "" {
 			http.Error(w, "Server not specified", http.StatusBadRequest)
@@ -100,7 +143,7 @@ func registerHandler(w http.ResponseWriter, r *http.Request) {
 		mutex.Unlock()
 		fmt.Fprintf(w, "Registered backend: %s\n", server)
 
-	case http.MethodDelete:
+	case http.MethodDelete: // Delete a registered backend
 		if server == "" {
 			http.Error(w, "Server not specified", http.StatusBadRequest)
 		}
@@ -136,6 +179,8 @@ func main() {
 
 func forwardReq(w http.ResponseWriter, r *http.Request) {
 	backend := selectHealthyBackend()
+
+	log.Printf("Current backend is: %s\n", backend)
 
 	// Create a new request to forward
 	req, err := http.NewRequest(r.Method, backend, r.Body)
@@ -174,5 +219,4 @@ func forwardReq(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		log.Printf("Error copying response body: %v", err)
 	}
-
 }
